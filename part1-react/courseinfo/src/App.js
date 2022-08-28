6e35b25bd6ff4b382a5d2efa9dabf2f8f69d28c1
Martin Cardozo <martin237@DESKTOP-DMLPOK8.localdomain>
2022-08-27 21:37:06 -0300

    Agregando componentes: Header, Content y Total

--- part1-react/courseinfo/src/App.js
+++ part1-react/courseinfo/src/App.js
@@ -1,13 +1,3 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  
 /**
  * One-Liner
  * const Header = ({course}) => <h1>{course}</h1>
@@ -16,20 +6,37 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
+const Content = (props) => {
+  return (
+    <p>
+      {props.part}  {props.exercise}
+    </p>
+  )
+}
 
+const Total = (props) => {
+  const tot = props.tot[0] + props.tot[1] + props.tot[2]
+  return (
+    <p>Number of exercises {tot}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total tot={[exercises1,exercises2,exercises3]} />
     </div>
   )
 }
